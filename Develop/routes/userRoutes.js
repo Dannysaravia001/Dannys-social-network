6bd595896d5ecbb9377b5065004d256fb8e9da30
unknown <dannysaravia100@gmail.com>
2024-03-15 19:02:41 -0400

    added get and post in userroutes.js

---
+++ Develop/routes/userRoutes.js
@@ -0,0 +1,40 @@
+// Initiate dependencies
+const express = require('express');
+const { User } = require('../models/User');
+const router = express.Router();
+
+// GET all users
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find().populate('thoughts').populate('friends');
+        res.json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET a single user by its _id
+router.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate('thoughts').populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// POST a new user
+router.post('/users', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
